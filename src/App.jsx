9e931d6d34769d554552b2114d8d964aaf2b0da8
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-04 22:26:50 +0900

    update toolbar, hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons'
+
+
+import useStyles from './styles';
+
+
 
 const App = () => {
+
+  const classes = useStyles();
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant='h6'>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              Hello everyone This is a photo album and I'm trying to make this sectence as long as possible so we can see how does it looks like on the screen.
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>                
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 };
 
