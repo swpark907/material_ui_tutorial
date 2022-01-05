35defec625621ea6150171054af47c92a367255c
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-01-05 11:42:14 +0900

    update image card, footer section & finish styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { PhotoCamera } from '@material-ui/icons'
 
 import useStyles from './styles';
 
-
+const cards= [1,2,3,4,5,6,7,8,9]
 
 const App = () => {
 
@@ -16,7 +16,7 @@ const App = () => {
       <CssBaseline />
       <AppBar position='relative'>
         <Toolbar>
-          <PhotoCamera />
+          <PhotoCamera className={classes.icon}/>
           <Typography variant='h6'>
             Photo Album
           </Typography>
@@ -31,7 +31,7 @@ const App = () => {
             <Typography variant='h5' align='center' color='textSecondary' paragraph>
               Hello everyone This is a photo album and I'm trying to make this sectence as long as possible so we can see how does it looks like on the screen.
             </Typography>
-            <div>
+            <div className={classes.buttons}>
               <Grid container spacing={2} justify='center'>
                 <Grid item>
                   <Button variant="contained" color="primary">
@@ -47,7 +47,44 @@ const App = () => {
             </div>
           </Container>
         </div>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          <Grid container spacing={4}>
+          {cards.map( (card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              
+              <Card className={classes.card}>
+                <CardMedia 
+                className={classes.cardMedia}
+                image='https://source.unsplash.com/random'
+                title='Image title'
+                />
+                <CardContent className={classes.cardContent} >
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. You can use this section to describe the content.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size='small' color='primary'  >View</Button>
+                  <Button size='small' color='primary'  >Edit</Button>
+                  
+                </CardActions>
+              </Card>
+            </Grid>
+            ) )}
+          </Grid>
+        </Container>
       </main>
+      <footer className={classes.footer}>
+            <Typography variant='h6' align='center' gutterBottom>
+              Fotter
+            </Typography>
+            <Typography variant='subtitle1' align='center' color='textSecondary'>
+              Something here to give the footer a purpose!
+            </Typography>
+      </footer>
     </>
   );
 };
